94e08a8d127c59a768fe8d43989b4fb684da81fa
AuliaAzkaA <19623257@std.stei.itb.sc.id>
2025-06-07 00:15:59 +0700

    feat: logout

--- src/main/java/id/sti/potek/App.java
+++ src/main/java/id/sti/potek/App.java
@@ -1,6 +1,6 @@
 package id.sti.potek;
 
-import id.sti.potek.ui.RegisterView;
+import id.sti.potek.ui.MainView;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
-        new RegisterView().start(primaryStage);
+        new MainView().start(primaryStage);
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/id/sti/potek/ui/LogoutView.java
@@ -0,0 +1,52 @@
+package id.sti.potek.ui;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class LogoutView {
+
+    public void start(Stage ownerStage) {
+        // Create the modal stage
+        Stage stage = new Stage(StageStyle.TRANSPARENT);
+        stage.initOwner(ownerStage);
+        stage.initModality(Modality.APPLICATION_MODAL);
+
+        // Close button
+        Button closeButton = new Button("✕");
+        closeButton.getStyleClass().add("close-button");
+        closeButton.setOnAction(e -> stage.close());
+
+        // Logout success message
+        Label message = new Label("Berhasil logout!");
+        message.getStyleClass().add("logout-message");
+
+        StackPane modalContent = new StackPane();
+        modalContent.getStyleClass().add("logout-modal");
+
+        VBox messageBox = new VBox(message);
+        messageBox.setAlignment(Pos.CENTER);
+
+        modalContent.getChildren().addAll(messageBox, closeButton);
+        StackPane.setAlignment(closeButton, Pos.TOP_RIGHT);
+        StackPane.setMargin(closeButton, new Insets(0));
+
+        StackPane root = new StackPane(modalContent);
+        root.getStyleClass().add("logout-root");
+        root.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(root, 400, 200);
+        scene.setFill(null);
+        scene.getStylesheets().add(getClass().getResource("/css/logout.css").toExternalForm());
+
+        stage.setScene(scene);
+        stage.show();
+    }
+}
